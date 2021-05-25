47edb6ac0ab6f8ea07afc3336b809e41829b74a5
avdeevcv1978 <avdeevcv1978@mail.ru>
2021-05-26 00:03:19 +0300

    Добавили проект

---
+++ myapp/src/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+
+import Header from "./components/Header";
+import Main from "./components/Main/Main";
+import Footer from "./components/Footer";
+import ArrowTop from "./components/ArrowTop";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <Main />
+    <Footer />
+    <ArrowTop />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+document.querySelector(".ArrowTop").addEventListener("click", () => {
+  window.scrollTo(0, 0);
+});
+
+// document.querySelector("[href='#blog']").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   window.scrollTo(0, document.getElementById("catalog").offsetHeight);
+// });
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    document.querySelector(".ArrowTop").classList.add("animate__zoomIn");
+    document.querySelector(".ArrowTop").classList.remove("hidden");
+  } else {
+    document.querySelector(".ArrowTop").classList.remove("animate__zoomIn");
+    document.querySelector(".ArrowTop").classList.add("hidden");
+  }
+});
+
+document.getElementById("categoryList").addEventListener("input", () => {
+  const catList = document.getElementById("categoryList");
+  showCards(catList.value);
+});
+
+function showCards(catId) {
+  for (let card of document.querySelectorAll(".card")) {
+    if (catId === "all") {
+      card.classList.remove("hidden");
+    } else {
+      if (catId === card.dataset.category) {
+        card.classList.remove("hidden");
+      } else {
+        card.classList.add("hidden");
+      }
+    }
+  }
+}
